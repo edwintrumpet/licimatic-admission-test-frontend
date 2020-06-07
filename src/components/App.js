b4e3199a57d580915d978dd0f0eb7ee9063af8de
Edwin García <spark.com.co@gmail.com>
2020-06-07 17:31:44 -0500

    Set styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import GlobalStyles from '../styles/GlobalStyles'
 
 export default function App() {
     return (
+      <>
+        <GlobalStyles />
         <h1>React works!</h1>
+      </>
     )
 }
